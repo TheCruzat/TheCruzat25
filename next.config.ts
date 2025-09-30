88b952b4a856c2fb75193a30c5bdfcc88aef7b8a
Dan Cruzat <thecruzat@gmail.com>
2025-09-30 09:25:35 -0700

    Turbopack config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,16 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
 
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: ['@svgr/webpack'],
-      });
-      return config;
-    },
-
+    turbopack: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.ts',
+        },
+      },
+    }
 };
 
 export default nextConfig;
