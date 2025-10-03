963a42acca234875171c9a0fbf9e62069c7c60d8
Dan Cruzat <thecruzat@gmail.com>
2025-10-02 20:58:16 -0700

    Frame: To border, or not to border?

--- src/app/components/frame.tsx
+++ src/app/components/frame.tsx
@@ -1,3 +1,5 @@
+// module to pull together Intro, Content and Finale
+
 import PageIntro from './page-intro';
 import PageContent from './page-content';
 import PageFinale from './page-finale';
@@ -16,16 +18,14 @@ export default function Frame({ content }: FrameContent) {
         top-[--frameY] bottom-[--frameY]
         left-[--frameX] right-[--frameX]
         lg:left-[12vw] lg:right-[12vw]
-
         md:rounded-[2.5rem]
-
         shadow-[0_0_2.75rem_rgba(0,0,0,0.4)]
         inset-shadow-[rgba(0,0,0,0.4)]
         md:backdrop-blur-[10px]
         border-0
         border-white
         max-md:border-x-0
-      "> {/* max-md:border-t-[2px] max-md:border-b-[2px] md:border-[2px] border-[#cccccc] */}
+      ">
 
         <PageIntro />
 
