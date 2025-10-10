28713a9e6d8148b75717d3f99abc51f4f6490d08
Dan Cruzat <thecruzat@gmail.com>
2025-10-10 00:23:56 -0700

    Footer CSS fixes

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -22,7 +22,7 @@ export default function Footer({ nav, socials, bg }: FootProps) {
     md:rounded-[1.5rem]
     md:mb-[var(--frameY)]
     max-md:absolute
-    max-md:bottom-[-242px]
+    max-md:bottom-[-242px]  // offset for closed mobile
     max-md:left-24
     max-md:right-24
     max-sm:left-12
@@ -47,11 +47,11 @@ export default function Footer({ nav, socials, bg }: FootProps) {
   // mobile nav button
   const ToggleMobileTrigger = () => {
     return (
-      <div className="max-md:mb-[1rem] hidden max-md:block">
+      <div className="max-md:mb-[1rem] md:hidden max-md:block">
         <button
           aria-label="Toggle mobile menu"
           className="
-          w-[3rem]
+          w-[100%]
           h-[3rem]
           rotate-[180deg]
           duration-200
@@ -60,6 +60,8 @@ export default function Footer({ nav, socials, bg }: FootProps) {
           <svg
             className="
               max-w-[100%]
+              w-[3rem]
+              h-[3rem]
               " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M297.4 438.6C309.9 451.1 330.2 451.1 342.7 438.6L502.7 278.6C515.2 266.1 515.2 245.8 502.7 233.3C490.2 220.8 469.9 220.8 457.4 233.3L320 370.7L182.6 233.4C170.1 220.9 149.8 220.9 137.3 233.4C124.8 245.9 124.8 266.2 137.3 278.7L297.3 438.7z" fill="white" /></svg>
         </button>
       </div>
